41639edc9c89f52e3c694b77a8a16e688bdcfa07
Jet-Tsyn Lee <jlee@yozu.co.uk>
2019-01-28 13:56:24 +0000

    Video 17

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -14,13 +14,17 @@ class App extends React.Component {
   addFish = fish => {
     const fishes = { ...this.state.fishes };
     fishes[`fish${Date.now()}`] = fish;
-    this.setState({
-      fishes: fishes
-    });
+    this.setState({ fishes });
   };
 
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
+  };
+
+  addToOrder = key => {
+    const order = { ...this.state.order };
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
   };
 
   render() {
@@ -30,11 +34,16 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" age={100} />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,17 +2,28 @@ import React from "react";
 import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
+  // handleClick = () => {
+  //   this.props.addToOrder(this.props.index);
+  // };
+
   render() {
     const { image, name, price, desc, status } = this.props.details;
+    const isAvailble = status === "available";
+
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
         <h3 className="fish-name">
           {name}
           <span className="price">{formatPrice(price)}</span>
-          <p>{desc}</p>
-          <bitton>Add To Cart</bitton>
         </h3>
+        <p>{desc}</p>
+        <button
+          disabled={!isAvailble}
+          onClick={() => this.props.addToOrder(this.props.index)}
+        >
+          {isAvailble ? "Add To Order" : "Sold Out"}
+        </button>
       </li>
     );
   }
